82898c9bd9b80bfa4b393fed98dc5c31212668d8
Hector Ccollatupa <hccollatupa@gmail.com>
2021-04-22 16:04:00 -0500

    Valor de cada carta

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -43,3 +43,23 @@ const perdirCarta = () => {
 }
 
 //perdirCarta();
+const valorCarta = (carta) => {
+    const valor = carta.substring(0, carta.length - 1);
+    return (isNaN(valor)) ?
+        (valor === 'A') ? 11 : 10 :
+        valor * 1;
+    /*
+    let puntos = 0;
+    if (isNaN(valor)) {
+        puntos = (valor === 'A') ? 11 : 10;
+        console.log('No es un número');
+    } else {
+        console.log('Es un número');
+        puntos = valor * 1;
+    }
+    console.log(puntos);
+    */
+}
+
+const valor = valorCarta(perdirCarta());
+console.log({ valor });
